2cdc74d6350b4c6113b24c431b28b22640027cb0
Thomas Bruckmaier (bob-gross) <thomasbruckmaier@web.de>
2014-10-27 13:07:26 +0200

    Changed fancybox layout configuration

--- javascript/GalleryPage.js
+++ javascript/GalleryPage.js
@@ -1,5 +1,13 @@
-;(function($) {
-	$(document).ready(function() { 
-		$(".fancybox").fancybox();
-	});
+(function($){
+    $(document).ready(function() {
+        $('.fancybox').fancybox({
+            openEffect: 'none',
+            closeEffect: 'none',
+            nextEffect: 'none',
+            prevEffect: 'none',
+            closeBtn: false,
+            arrows: false,
+            nextClick: true
+        });
+    });
 }(jQuery));
